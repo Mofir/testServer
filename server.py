760586eb3775ccf668e6fc42887fdfd5e9ac13fa
Mohammad Afif <mohammadafif64@gmail.com>
2019-10-08 18:09:33 +0900

    update

--- server.py
+++ server.py
@@ -5,16 +5,26 @@ my_port = 19237
 
 @app.route('/', methods=['GET'])
 def get_html():
-    return render_template('./humidtemp.html')
+    try:
+        f = open(file_path, 'r')
+        for row in f:
+            dht = row
+    except Exception as e:
+        print(e)
+        return e
+    finally:
+        f.close()
+    values = dht.split(',')
+    return render_template('./index.html', time = values[0], humidity = values[1], temperature = values[2])
 
 @app.route('/dht', methods=['POST'])
 def update_dht():
     time = request.form["time"]
-    dht = request.form["dht"]
-    #temperature = request.form["temperature"]
+    humidity = request.form["humidity"]
+    temperature = request.form["temperature"]
     try:
         f = open(file_path, 'w')
-        f.write(time + "," + dht)
+        f.write(time + "," + humidity + "," + temperature)
         return "Success to write!"
     except Exception as e:
         print(e)
@@ -28,7 +38,7 @@ def get_dht():
         f = open(file_path, 'r')
         for row in f:
             dht = row
-        return dht
+        return dht.split(',')
     except Exception as e:
         print(e)
         return e
