4e69f6a045c1c9ad9618ee41a37f3a61d4ed339b
Mohammad Afif <mohammadafif64@gmail.com>
2019-10-10 13:05:48 +0900

    update

--- server.py
+++ server.py
@@ -20,12 +20,12 @@ def get_html():
 @app.route('/dht', methods=['POST'])
 def update_dht():
     time = request.form["time"]
-    #dht = request.form["dht"]
-    humidity = request.form["humidity"]
-    temperature = request.form["temperature"]
+    dht = request.form["dht"]
+    #humidity = request.form["humidity"]
+    #temperature = request.form["temperature"]
     try:
         f = open(file_path, 'w')
-        f.write(time + "," + humidity + ","+ temperature)
+        f.write(time + "," + dht)
         return "Success to write!"
     except Exception as e:
         print(e)
